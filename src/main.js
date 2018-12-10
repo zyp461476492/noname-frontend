e10929e37bd97205c2739fe651ced90016789b77
zhangyp <461476492@qq.com>
2018-12-10 16:41:23 +0800

    feat: 添加日期过滤器

--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
 import Vue from 'vue';
 import RouterVue from './views/router.vue';
 // import App from './views/App.vue';
+import {formatDate} from '@/plugins/dateFormate.js';
 import router from "./router";
 import axios from "axios";
 import "./plugins/vuetify";
 
 Vue.prototype.$axios = axios;
 Vue.config.productionTip = false
+
+// 添加全局过滤器
+Vue.filter('formateDate', function (value) {
+  let date = new Date(value);
+  return formatDate(date, 'yyyy-MM-dd hh:mm:ss')
+})
 
 new Vue({
   router,
